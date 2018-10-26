0926a28d6f65a325d7d6b0286890367af9a549d3
charlie <charlescc9@yahoo.com>
2018-10-26 10:41:08 -0700

    Perception project

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -85,16 +85,48 @@ def pcl_callback(ros_cloud):
     seg.set_distance_threshold(max_distance)
     inliners, coefficients = seg.segment()
 
-    # TODO: Extract inliers and outliers
+    # Extract inliers and outliers
+    cloud_table = cloud_filtered.extract(inliners, negative=False)
+    cloud_objects = cloud_filtered.extract(inliners, negative=True)
 
-    # TODO: Euclidean Clustering
+    # Convert PCL data to ROS messages
+    ros_cloud_table = pcl_to_ros(cloud_table)
+    ros_cloud_objects = pcl_to_ros(cloud_objects)
 
-    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # Euclidean Clustering
+    white_cloud = XYZRGB_to_XYZ(cloud_objects)
+    tree = white_cloud.make_kdtree()
+
+    # Create Cluster-Mask Point Cloud to visualize each cluster separately
+    ec = white_cloud.make_EuclideanClusterExtraction()
+    ec.set_ClusterTolerance(0.05)
+    ec.set_MinClusterSize(10)
+    ec.set_MaxClusterSize(10000)
+    ec.set_SearchMethod(tree)
+    cluster_indices = ec.Extract()
+
+    # Assign a color corresponding to each segmented object in scene
+    cluster_color = get_color_list(len(cluster_indices))
+    color_cluster_point_list = []
+    for j, indices in enumerate(cluster_indices):
+        for i, index in enumerate(indices):
+            color_cluster_point_list.append([white_cloud[index][0],
+                                             white_cloud[index][1],
+                                             white_cloud[index][2],
+                                             rgb_to_float(cluster_color[j])])
+
+    # Create clusters based on color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
 
     # Convert PCL data to ROS messages
     cloud_filtered_ros = pcl_to_ros(cloud_filtered)
+    cluster_cloud_ros = pcl_to_ros(cluster_cloud)
 
     # Publish ROS messages
+    table_pub.publish(ros_cloud_table)
+    objects_pub.publish(ros_cloud_objects)
+    cluster_pub.publish(cluster_cloud_ros)
     filtered_pub.publish(cloud_filtered_ros)
 
 # Exercise-3 TODOs:
@@ -173,12 +205,12 @@ if __name__ == '__main__':
     pcl_sub = rospy.Subscriber('/pr2/world/points', pc2.PointCloud2, pcl_callback, queue_size=1)
 
     # Create Publishers
-    # pcl_objects_pub = rospy.Publisher('/pcl_objects', PointCloud2, queue_size=1)
-    # pcl_table_pub = rospy.Publisher('/pcl_table', PointCloud2, queue_size=1)
-    # pcl_cluster_pub = rospy.Publisher('/pcl_cluster', PointCloud2, queue_size=1)
     # object_markers_pub = rospy.Publisher('/object_markers', Marker, queue_size=1)
     # detected_objects_pub = rospy.Publisher('/detected_objects', DetectedObjectsArray, queue_size=1)
+    cluster_pub = rospy.Publisher('/clusters', PointCloud2, queue_size=1)
     original_pub = rospy.Publisher('/objects_original', PointCloud2, queue_size=1)
+    objects_pub = rospy.Publisher('/pcl_objects', PointCloud2, queue_size=1)
+    table_pub = rospy.Publisher('/pcl_table', PointCloud2, queue_size=1)
     filtered_pub = rospy.Publisher('/objects_filtered', PointCloud2, queue_size=1)
 
     # Load Model From disk
